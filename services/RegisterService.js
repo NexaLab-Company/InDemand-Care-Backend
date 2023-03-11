27e4a9ea5433e787529f0f17e01711c8ac1f4754
Afshal Hassan <afshal.hassan@venturedive.com>
2023-03-11 09:46:50 +0500

    Login SignUp updated

--- services/RegisterService.js
+++ services/RegisterService.js
@@ -4,38 +4,67 @@ const GenericResponse = require("../dto/GenericResponse");
 const hashPass = require('../utils/hashPassword')
 
 
-module.exports =  async (req,res) => {
+module.exports = async (req, res) => {
 
-  const {name, email, password, role} = req.body
+
+
+  const { name, email, password, role } = req.body
+
+
   const query = 'SELECT * FROM users WHERE email = ?';
-  db.query(query,[email],(err,result)=>{
-    if (err){
 
-      return res.status(500).send(new GenericResponse("Error in inserting user" , err ));
 
-    } else {
-      if(result.length > 0){
+  db.query(query, [email], (err, result) => {
 
-        return res.status(409).send(new GenericResponse("User already exists" , null ));
 
-      } else{
+    if (err) {
+
+      return res.status(500).send(new GenericResponse("Error in inserting user", err));
+
+    }
+
+
+    else {
+
+      if (result.length > 0) {
+
+        return res.status(409).send(new GenericResponse("User already exists", null));
+
+      }
+
+
+      else {
+
+
         const insertQuery = 'INSERT INTO users (name, email, role, password) VALUES (?, ?, ?, ?)';
-        hashPass(password,(hash,error)=>{
 
-          if (error){
+        hashPass(password, (hash, error) => {
 
-            return res.status(409).send(new GenericResponse("Error in hashing password" , error ));
 
-          } else {
-            db.query(insertQuery, [name, email, role, hash ], (error, results) => {
-              if(error){
-    
-                return res.status(500).send(new GenericResponse("Error in inserting user" , error ));
-      
-              } else {
-    
-                return res.status(200).send(new GenericResponse("User succesfully added" , results ));
-      
+          if (error) {
+
+            return res.status(409).send(new GenericResponse("Error in hashing password", error));
+
+          }
+
+
+          else {
+
+
+            db.query(insertQuery, [name, email, role, hash], (error, results) => {
+
+
+              if (error) {
+
+                return res.status(500).send(new GenericResponse("Error in inserting user", error));
+
+              }
+
+
+              else {
+
+                return res.status(200).send(new GenericResponse("User succesfully added", results));
+
               }
             })
           }
